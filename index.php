c257833831f9f538f4806350fee2d9408a700742
codeSmith13 <peeperz13@gmail.com>
2017-09-05 20:20:17 -0500

    commiting First index.php

--- index.php
+++ index.php
@@ -1,3 +1,71 @@
 <?php
 
 ?>
+ <!DOCTYPE html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Full Stack BHM</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/4.2.0/normalize.min.css">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+        <!--<link rel="stylesheet" href="css/main.css"> -->
+
+    </head>
+    <body>
+     <header class="container-fluid hero" >
+     	<div clss="row">
+     	<nav id="navBar">
+     		<ul>
+     			<li>Home</li>
+     			<li>Alumni</li>
+     			<li>Events</li>
+     			<li>FS-Github</li>
+     			<li>Contact Us</li>
+     		</ul>
+		</nav>
+			<div class="imgBackground"> 
+				<img src="backgrond">
+			</div>
+
+			<div class="mainText">
+				<img src="logo">
+				<h1>The New CAREER CHANGING Opportunity</h1>
+			</div>
+			</div>
+	 </header>
+
+	 <div class="container">
+	 	<div class="row">
+	 	<h2>WHAT IS FULL STACK?</h2>
+	 		<P>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit at culpa veniam voluptatem aut cumque mollitia delectus. Deserunt incidunt totam minus aliquid, commodi id. Consequuntur quaerat illum harum quis pariatur.</P>
+	 	
+	 	<h2>WHO IS FULL STACK FOR?</h2>
+	 		<P>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi sed repellat odio expedita perferendis officia veniam cum pariatur sunt, explicabo ad illo voluptatibus, rem magni veritatis quos eos officiis accusamus.</P>
+	 	</div>
+	 </div>
+
+
+     	
+     		
+     	
+
+        <script src="http://code.jquery.com/jquery-1.12.4.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.4.min.js"><\/script>')</script>
+
+        <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+        <script>
+            window.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;
+            ga('create','UA-XXXXX-Y','auto');ga('send','pageview')
+        </script>
+        <script src="https://www.google-analytics.com/analytics.js" async defer></script>
+    </body>
+</html>
