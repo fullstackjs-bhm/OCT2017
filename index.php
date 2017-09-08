558c8436dd2b3b90f47c79c15b1695a0606cbd07
codeSmith13 <peeperz13@gmail.com>
2017-09-07 19:55:15 -0500

    update all

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
 
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
-        <!--<link rel="stylesheet" href="css/main.css"> -->
+        <link rel="stylesheet" href="css/style.css">
 
     
       <div id="header">
