0ea8e308b695ec704fba8c1c8b076cbf4080ecb8
Brad Bales <brad4au57@gmail.com>
2017-09-11 21:08:41 -0500

    Updated js and index

--- index.php
+++ index.php
@@ -3,87 +3,84 @@
 ?>
 
 
- <!DOCTYPE html>
+<!DOCTYPE html>
 <html class="no-js" lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title>Full Stack BHM</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Full Stack BHM</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
         
-        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
-        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+    <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+    <link rel="apple-touch-icon" href="apple-touch-icon.png">
 
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/4.2.0/normalize.min.css">
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
-
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
-        <link rel="stylesheet" href="css/style.css">
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/4.2.0/normalize.min.css">
     
-      <div id="header">
-       
-        </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
 
-
-             <!--hero area-->
-
-<div id="hero" class="container-fluid">
-<div class="container">
-          <header class="row">
-            <div class="col-12 col-md-4 mt-2">
-        
-            </div>
-            <div class="col-md-8">
-              <nav class="text-right mt-1">
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Alumni</a></li>
-                <li><a href="#">Events</a></li>
-                <li><a href="#">FS-GitHub</a></li>
-                <li><a href="#">Contact Us</a></li>
-              </nav>
-            </div>
-          </header>
-         </div>
-   <div class="container mb-4">
-		<div class="row">
-<div class="col-12 text-center">
-        		<div class="col-12">
-            		<img src="img/assets/bhmLogo.png">
-            		<h2 class="text-center">The New CAREER CHANGING Opportunity </h2>
-        		</div>
-		</div>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>   
+    <div id="hidden-header">
+         
     </div>
-</div>
 
 
-	 <div class="container col-md-12">
-	 	<div class="row">
-	 		<div class="col-6">
-	 			<h2>WHAT IS FULL STACK?</h2>
-	 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit at culpa veniam voluptatem aut cumque mollitia delectus. Deserunt incidunt totam minus aliquid, commodi id. Consequuntur quaerat illum harum quis pariatur.</p>
-	 	    </div>
-	 	    <div class="col-6">
-	 			<h2>WHO IS FULL STACK FOR?</h2>
-	 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit at culpa veniam voluptatem aut cumque mollitia delectus. Deserunt incidunt totam minus aliquid, commodi id. Consequuntur quaerat illum harum quis pariatur.</p>
-	 	    </div>
-	 	</div>
-	 </div>
+  <!--     hero area     -->
 
+    <div id="hero" class="container-fluid">
+      <div class="container">
+        <header class="row">
+          <div class="col-12 col-md-4 mt-2">
+            
+          </div>
+          <div class="offset-4 col-md-8">
+            <nav class="text-right mt-1">
+              <li><a href="#">Home</a></li>
+              <li><a href="#">Alumni</a></li>
+              <li><a href="#">Events</a></li>
+              <li><a href="#">FS-GitHub</a></li>
+              <li><a href="#">Contact Us</a></li>
+            </nav>
+          </div>
+        </header>
+      </div>
+      <div class="container mb-4">
+    		<div class="row">
+          <div class="col-12 text-center">
+          	<div class="col-12">
+           		<img src="img/assets/bhmLogo.png">
+              <h2 class="text-center">The New CAREER CHANGING Opportunity </h2>
+            </div>
+    		  </div>
+        </div>
+      </div>
+    	<div class="container col-md-12">
+    	 	<div class="row">
+    	 		<div class="col-6">
+    	 			<h2>WHAT IS FULL STACK?</h2>
+    	 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit at culpa veniam voluptatem aut cumque mollitia delectus. Deserunt incidunt totam minus aliquid, commodi id. Consequuntur quaerat illum harum quis pariatur.</p>
+    	 	  </div>
+    	 	  <div class="col-6">
+    	 			<h2>WHO IS FULL STACK FOR?</h2>
+    	 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit at culpa veniam voluptatem aut cumque mollitia delectus. Deserunt incidunt totam minus aliquid, commodi id. Consequuntur quaerat illum harum quis pariatur.</p>
+    	 	  </div>
+    	 	</div>
+    	</div>    	
 
-     	
-     		
-     	
+<!--  End of page scripts  -->
+    <script src="http://code.jquery.com/jquery-1.12.4.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.4.min.js"><\/script>')</script>
 
-        <script src="http://code.jquery.com/jquery-1.12.4.min.js"></script>
-        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.4.min.js"><\/script>')</script>
+   <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+    <script>
+      window.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;
+      ga('create','UA-XXXXX-Y','auto');ga('send','pageview')
+    </script>
+    <script src="https://www.google-analytics.com/analytics.js" async defer></script>
 
-        <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
-        <script>
-            window.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;
-            ga('create','UA-XXXXX-Y','auto');ga('send','pageview')
-        </script>
-        <script src="https://www.google-analytics.com/analytics.js" async defer></script>
-    </body>
+  </body>
 </html>
---
+++ js/navMenu.js
@@ -0,0 +1,21 @@
+var navBar = $('#navBar');
+var topofNav = navBar.offset().top; //gets offset of header
+var height = navBar.outerHeight(); //gets height of header
+var navSlide = true;
+
+$(window).scroll(function(){
+  if($(this).scrollTop() > (topofNav + height) && navSlide){
+    $('#scrollNav').stop().animate({top: 0},500);
+    navSlide = false;
+  }
+  else if($(window).scrollTop() < (topofNav + height) && !navSlide){
+    $('#scrollNav').stop().animate({top: -55}, 500);
+    navSlide = true;
+    $('.collapsedNav').hide(); //Rehides the collapsed nav menu once you scroll back to the top
+  }
+});
+
+//Shows or hides the collapsed nav menu
+$('#menuButton').on('click',function(){
+  $('.collapsedNav').toggle();
+});
