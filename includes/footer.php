bd3bcfb0e7fb873fab081e764aa9f62683bf1363
codeSmith13 <peeperz13@gmail.com>
2017-09-21 19:27:10 -0500

    footer step one

--- includes/footer.php
+++ includes/footer.php
@@ -14,5 +14,51 @@
     <!-- navMenu Script -->
     <script type="text/javascript" src="js/navMenu.js"></script>
 
+
+    <!-- Footer -->
+     <div class="container-fluid"> -->
+      <div class="row">
+        <div class="col-3">
+          <h2>About</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <div class="col-3">
+          <h2>mscdkdsk</h2>
+            <ul>
+              <li>1</li>
+              <li>2</li>
+              <li>3</li>
+              <li>3</li>
+              <li>4</li>
+            </ul>
+        </div>
+        <div class="col-3">
+          <h2>fdslkjfdskl</h2>
+            <img src="http://via.placeholder.com/140x100" alt="">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+              consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+              proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <div class="col-3">
+          <h2>Contact</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        
+      </div>
+    </div>
+
   </body>
 </html>
