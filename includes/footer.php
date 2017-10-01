e73f477a2639f1ffd82f60156914f1ad572a3870
Steven Andrews <sandrewsjscc@gmail.com>
2017-10-01 18:55:14 -0500

    Added actual content and made some slight coding changes to fix alignment issues.

--- includes/footer.php
+++ includes/footer.php
@@ -4,12 +4,7 @@
           <div class="row">
             <div class=" col-12 col-md-4 mt-3 footerCol">
               <h2>About</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <p>The Full Stack Coding program is a fifteen week immersive course in web development. It has been designed to aid participants in gaining the skills necessary to secure an entry-level position as a web developer in today’s job market. The training is focused on HTML, CSS, JavaScript, MySQL, and PHP, as these languages are some core fundamental languages to develop for the World Wide Web.</p>
             </div>
             <div class="col-12 col-md-5 mt-3 footerCol">
               <div class="row">
@@ -18,7 +13,7 @@
                   <a href="https://www.google.com/maps/place/Jefferson+State+Community+College/@33.3855846,-86.7334231,15z/data=!4m2!3m1!1s0x0:0xd4c37f71253fa12?sa=X&ved=0ahUKEwj_vNeZhsTWAhXCTSYKHcpmBBkQ_BIIfDAK" target="_blank"/><img class="img-fluid" src="img/mapss.png" alt=""/></a>
                 </div>
                 <div class="col-6">
-                  <p class="address pt-md-4 text-left">4600 Valleydale Road <br>Birmingham, Alabama 35242 <br>(205) 983-5911/(800) 239-5900</p>
+                  <p class="address pt-md-4 text-left">4600 Valleydale Road <br>Birmingham, Alabama 35242 <br>(205) 983-5911 <br /> (800) 239-5900</p>
                 </div>
               </div>
               <div class="row">
@@ -26,7 +21,7 @@
                   <a href="https://www.google.com/maps/place/Jefferson+State+Community+College/@33.6525536,-86.7082145,15z/data=!4m2!3m1!1s0x0:0xb67ff201abd54339?sa=X&ved=0ahUKEwju1NKYiMTWAhUBxiYKHRZDCnAQ_BIIiQEwCg" target="_blank"/><img class="img-fluid" src="img/mapsj.png" alt=""/></a>
                 </div>
                 <div class="col-6">
-                  <p class="address pt-md-4 text-left">2601 Carson Road <br>Birmingham, Alabama 35215 <br>(205) 853-1200/(800) 239-5900</p>
+                  <p class="address pt-md-4 text-left">2601 Carson Road <br>Birmingham, Alabama 35215 <br>(205) 853-1200 <br /> (800) 239-5900</p>
                 </div>
               </div>
             </div>
@@ -39,9 +34,9 @@
             </div>
           </div>
 
-          <div class="row copyTerms">
-            <div class="col-6 text-left">&copy; copyright 2017/2018</div>
-            <div class="col-6 text-right"><a href="#">Terms &amp; Conditions</a> | <a href="#">Privacy Policy</a></div>
+          <div class="row py-3 copyTerms">
+            <div class="col-12 col-md-6 text-center text-md-left">&copy; Copyright <?php echo date('Y'); ?></div>
+            <div class="col-12 col-md-6 text-center text-md-right"><a href="#">Terms &amp; Conditions</a> | <a href="#">Privacy Policy</a></div>
           </div>
       </div>
     </div>
--- index.php
+++ index.php
@@ -6,11 +6,15 @@
     	 	<div class="row">
     	 		<div class="col-12 col-md-6">
     	 			<h2 class="col-12 text-center mt-3 underline">WHAT IS FULL STACK?</h2>
-    	 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit at culpa veniam voluptatem aut cumque mollitia delectus. Deserunt incidunt totam minus aliquid, commodi id. Consequuntur quaerat illum harum quis pariatur.</p>
+    	 			<p>The Full Stack Coding program is a fifteen week immersive course in web development. It has been designed to aid participants in gaining the skills necessary to secure an entry-level position as a web developer in today’s job market. The training is focused on HTML, CSS, JavaScript, MySQL, and PHP, as these languages are some core fundamental languages to develop for the World Wide Web.</p>
+            <p>By this course being a part of JSCC Career Development Department, students Do Not receive credit hours towards any academics or college courses of any kind. This is a course strictly for personal growth and development of all individuals who desire to attend the Full Stack Coding training.
+            </p>
     	 	  </div>
     	 	  <div class="col-12 col-md-6">
     	 			<h2 class="col-12 text-center mt-3 underline">WHO IS FULL STACK FOR?</h2>
-    	 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit at culpa veniam voluptatem aut cumque mollitia delectus. Deserunt incidunt totam minus aliquid, commodi id. Consequuntur quaerat illum harum quis pariatur.</p>
+    	 			<p>If any individual desires to change careers or take on a new challenge in the information technology industry as a web developer, well this is the course that makes it possible. One can acquire the necessary knowledge very quickly, without two and four years of normal academic commitment, and gain the skills they will need to pursue an ever growing and expanding career in the world of Information Technology.</p>
+
+            <p>With entry-level salaries starting at $40k per year, web development has become one of the most highly sought after skills today. Employers are always looking for new talent in this field, as it is and will be in-demand a great deal as we progress in the future.</p>
     	 	  </div>
     	 	</div>
     	</div> 
@@ -30,20 +34,26 @@
       </div>
 
      <!-- infoArea&form -->
-      <div class="container-fluid mt-5 infoArea"> 
-       <div class="row">
-          <div class="col-12 col-md-6 tests mt-2">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt quisquam vel, <br> unde cupiditate. Voluptates consequuntur eos atque sequi quas unde at sed, veritatis, eligendi tempora earum quo, facilis vero quis! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, voluptates dicta rerum hic esse numquam, provident incidunt obcaecati sint sequi quasi magnam ducimus atque earum ea cumque ipsa accusamus voluptas!</p>
-          </div>
+      <div class="container-fluid mt-5 infoArea">
+        <div class="container"> 
+         <div class="row">
+            <div class="col-12 col-md-6 tests mt-2">
+              <h2>You Can Code Too!</h2>
+              <p>Ever wonder what it would be like to harness the web at your finger tips? Want to demystify what computer programming or coding is? Join us in the Full Stack Program! Want some one to contact you?</p>
+              <p>
+                Don't let HTML, CSS, and JS remain foreign to you. Learn to process information also know as "data" using PHP &amp; MySQL today.
+              </p>
+            </div>
 
-          <div class="col-12 col-md-6 text-center form">
-            <h2>Want More Info?</h2>
-            <h2><span>Complete This Form!</span></h2>
-            <form action="scripts/sendContact.php" method="POST">
-              <input class="col-12 mt-1 name" placeholder="name" type="text" name="firstname">
-              <input class="col-12 mt-1  email" placeholder="email" type="text" name="email">
-              <input class= "col-12 mt-1 btn btn-success text-center" type="submit" value="Submit">
-            </form>  
+            <div class="col-12 col-md-6 text-center form">
+              <h2>Want More Info?</h2>
+              <h2><span>Complete This Form!</span></h2>
+              <form action="scripts/sendContact.php" method="POST">
+                <input class="col-12 name" placeholder="Name" type="text" name="firstname">
+                <input class="col-12 my-2  email" placeholder="Email Address" type="text" name="email">
+                <input class= "col-12 btn btn-success text-center" type="submit" value="Submit">
+              </form>  
+            </div>
           </div>
         </div>
       </div> 
@@ -53,12 +63,12 @@
         <div class="row mt-5">
          <h2 class="col-12 text-center">Upcoming Events</h2>
           <hr>
-            <div class="col-md-6 events">
-              <h3 class="event-title text-center">Meet &amp; Greet<span class="blue"> | </span><span class="date">Lorem</span></h3>
+            <div class="col-md-5 ml-md-auto events">
+              <h3 class="event-title text-center">Meet &amp; Greet<span class="blue"> | </span><span class="event-date">Oct. 10<sup>th</sup> @ 7pm</span></h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita maiores accusamus hic assumenda eum dolorem. Sequi, delectus cumque? Doloribus sint, iusto magni soluta excepturi cum eveniet explicabo officia totam repellendus?</p>
             </div>
-            <div class="col-md-6 events">
-              <h3 class="event-title text-center">Meet &amp; Greet<span class="blue"> | </span><span class="date">Lorem</span></h3>
+            <div class="col-md-5 ml-md-auto events">
+              <h3 class="event-title text-center">Peer Program<span class="blue"> | </span><span class="event-date">Oct. 30<sup>th</sup> @ 7pm</span></h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita maiores accusamus hic assumenda eum dolorem. Sequi, delectus cumque? Doloribus sint, iusto magni soluta excepturi cum eveniet explicabo officia totam repellendus?</p>
             </div>
         </div>
@@ -69,10 +79,12 @@
   <!-- git hub banner -->
   <div class="bottomBanner">
     <div class="container-fluid">
-      <div class="col-12">
-        <h1 class="text-center">Visit our GitHub Page! <br>Click Below</h1>
-        <h1 class="arrow text-center">&#x21E3;</h1>
-        <a href="https://github.com/fullstackjs-bhm/OCT2017" target="_blank"><img class="img-fluid" src="img/assets/GitHub image.png"></a>
+      <div class="row">
+        <div class="col-12 text-center">
+          <h1 class="text-center">Visit our GitHub Page! <br>Click Below</h1>
+          <h1 class="arrow">&#x21E3;</h1>
+          <a class="d-block" href="https://github.com/fullstackjs-bhm/OCT2017" target="_blank"><img class="img-fluid" src="img/assets/GitHub image.png"></a>
+        </div>
       </div>
     </div>
   </div>
